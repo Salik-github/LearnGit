8dfc54654f178d9cc1d4a12ffcb53a91b5e77c3a
Salik <mohammedsalik2511@gmail.com>
2025-01-21 22:55:09 +0530

    "checking up "

--- demo/src/test/java/com/automationtesingwebsite/pagefactory/homePage.java
+++ demo/src/test/java/com/automationtesingwebsite/pagefactory/homePage.java
@@ -8,17 +8,23 @@ import org.openqa.selenium.WebElement;
 
 public class homePage extends  basePage{
     
+    private final By slider = By.xpath("//div[contains(@class,'n2-ss-slider-')]");
+    private final By arrivals = By.cssSelector(".woocommerce-LoopProduct-link");
 
     public homePage(WebDriver driver)
     {
         super(driver);  
     }
-    private final By slider = By.xpath("//div[contains(@class,'n2-ss-slider-')]");
 
 
     public boolean  verifySlider()
     {
         List<WebElement> sliderelement = driver.findElements(slider);
         return sliderelement.size() ==3; 
+    }
+    public boolean verifyArrival()
+    {
+        List<WebElement> Arrivalelement = driver.findElements(arrivals);
+        return Arrivalelement.size() ==3; 
     }
 }
