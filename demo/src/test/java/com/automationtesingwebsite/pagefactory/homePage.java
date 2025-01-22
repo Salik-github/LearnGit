b9e611a635f803131ba66d753081d19da5bf2575
Salik <mohammadsalikcse@gmail.com>
2025-01-22 10:06:46 +0530

    local

--- demo/src/test/java/com/automationtesingwebsite/pagefactory/homePage.java
+++ demo/src/test/java/com/automationtesingwebsite/pagefactory/homePage.java
@@ -8,17 +8,21 @@ import org.openqa.selenium.WebElement;
 
 public class homePage extends  basePage{
     
+    private final By slider = By.xpath("//div[contains(@class,'n2-ss-slider-')]");
 
     public homePage(WebDriver driver)
     {
         super(driver);  
     }
-    private final By slider = By.xpath("//div[contains(@class,'n2-ss-slider-')]");
-
 
     public boolean  verifySlider()
     {
         List<WebElement> sliderelement = driver.findElements(slider);
-        return sliderelement.size() ==3; 
+        return sliderelement.size() ==4; 
+        
+    }
+    public void test()
+    {
+        
     }
 }
